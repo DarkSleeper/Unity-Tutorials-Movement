a5ce73ce604d04cb9c660dc2182d1b963085cde5
bary <bary@localhost>
2025-01-14 11:40:12 +0800

    t2: jumping

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -6,14 +6,41 @@ public class MovingSphere : MonoBehaviour
     float maxSpeed = 10f;
 
     [SerializeField, Range(0f, 100f)]
-    float maxAcceleration = 10f;
+    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
 
     Rigidbody body;
 
     Vector3 velocity, desiredVelocity;
 
+    bool desiredJump;
+
+    bool onGround;
+
+    int jumpPhase;
+
     void Awake() {
         body = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision) {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision(Collision collision) {
+        for (int i = 0; i < collision.contactCount; i++) {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= 0.9f;
+        }
     }
 
     void Update() {
@@ -22,13 +49,43 @@ public class MovingSphere : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        desiredJump |= Input.GetButtonDown("Jump");
     }
 
     void FixedUpdate() {
-        velocity = body.velocity;
-        var maxSpeedChange = maxAcceleration * Time.deltaTime;
+        UpdateState();
+
+        var acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        var maxSpeedChange = acceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        if (desiredJump) {
+            desiredJump = false;
+            Jump();
+        }
+
         body.velocity = velocity;
+
+        onGround = false;
+    }
+
+    void UpdateState() {
+        velocity = body.velocity;
+        if (onGround) {
+            jumpPhase = 0;
+        }
+    }
+
+    void Jump() {
+        if (onGround || jumpPhase < maxAirJumps) {
+            jumpPhase += 1;
+            var jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            if (velocity.y > 0f) {
+                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+            }
+            velocity.y += jumpSpeed;
+        }
     }
 }
